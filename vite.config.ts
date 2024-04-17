06a2242c6fa2b83f1ce4bdf255140c6dada1dd6a
Tahinaniaina <tahinaniaina02@gmail.com>
2024-04-17 21:10:35 +0300

    resove all git error

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-<<<<<<< HEAD
   base: "/",
-=======
-  base: "/portfolio/",
->>>>>>> 3026c8bb2c7a2d0eb3be5bdf2a1b126c6832e074
 });
