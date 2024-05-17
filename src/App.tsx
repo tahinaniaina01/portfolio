bb7ead100f3caf9a304d82dc110a2abf3a86f5c5
tahinaniaina01 <tahinaniaina01@gmail.com>
2024-05-17 08:24:58 +0300

    uncomment ParticlesBackground section

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-// import ParticlesBackground from "./components/ParticlesBackground";
+import ParticlesBackground from "./components/ParticlesBackground";
 import Project from "./components/Project";
 import Skills from "./components/Skills";
 import { ThemeProvider } from "./components/theme-provider";
@@ -11,7 +11,7 @@ import { ThemeProvider } from "./components/theme-provider";
 export default function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      {/* <ParticlesBackground /> */}
+      <ParticlesBackground />
       <Header />
       <Hero />
       <AboutMe />
